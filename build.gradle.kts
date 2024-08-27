e97364056480de429704e2a4b8cfcc88a75cc390
Arian Chlebosz <chlebosz.adrian@gmail.com>
2024-08-27 23:36:13 +0200

    Bump Spring Boot and Spring dependency management to the most recent versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
-    id("org.springframework.boot") version "3.2.5"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "com.breader"
